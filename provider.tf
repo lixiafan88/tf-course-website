19106677e080e7500aeabc8e0a093896e27da620
lixiafan88 <lixiafan88@gmail.com>
2023-11-10 19:51:44 +0000

    ran terraform fmt

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket  = "roidtc-projects-213-tf-state"
-    prefix  = "terraform/state"
+    bucket = "roidtc-projects-213-tf-state"
+    prefix = "terraform/state"
   }
 
   required_providers {
